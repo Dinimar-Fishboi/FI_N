d6708899d05ddd1f6fb6118a9fda9d2ccd0bc4da
Dinimar Fish <tayla@tallisman.com.au>
2021-12-07 23:45:14 +1000

    added new page for backstory and further info

---
+++ client/src/Components/pages/More.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+const More = () => {
+
+    return (
+        <>
+        <Container>
+        <h1 style={{marginTop: "1rem"}}>
+            References and where to find more info:
+        </h1>
+        <p>
+            This will be where TIO related work goes
+        </p>
+        <p>
+            This will be where NBN related stuff happens.
+        </p>
+        </Container>
+        </>
+    )
+};
+
+export default More
